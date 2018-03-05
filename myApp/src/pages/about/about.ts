85ecfae486fec4d7ef4aea306108a503e41ce2c8
Antonio Bustamante Mirayo <abmirayo@gmail.com>
2018-03-05 14:38:14 -0800

    ionic experiments

---
+++ myApp/src/pages/about/about.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+  public base64Image: string;
+
+  constructor(public navCtrl: NavController, private camera: Camera) {
+
+  }
+
+
+  takePhotox(){
+    const options : CameraOptions = {
+      quality: 50, // picture quality
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options) .then((imageData) => {
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
+
